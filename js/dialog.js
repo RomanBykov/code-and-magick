88f75e852d7a05954f995f6e097504e51d02d5f2
r.bykov <r.bykov@babyblog.team>
2019-04-04 15:15:37 +0300

    module6-task1

--- js/dialog.js
+++ js/dialog.js
@@ -3,7 +3,11 @@
 (function () {
   var setupWindow = document.querySelector('.setup');
   var setupWindowUserPic = setupWindow.querySelector('.setup-user-pic');
+  var shopElement = setupWindow.querySelector('.setup-artifacts-shop');
+  var artifactsElement = setupWindow.querySelector('.setup-artifacts');
+  var draggedItem = null;
 
+  // перетаскивание окна настройки волшебника
   var dialogMoveHandler = function (evt) {
     evt.preventDefault();
 
@@ -33,7 +37,6 @@
         return result + 'px';
       };
 
-
       setupWindow.style.top = setNewCoords();
       setupWindow.style.left = setNewCoords(true);
     };
@@ -47,6 +50,43 @@
     document.addEventListener('mousemove', mouseMoveHandler);
     document.addEventListener('mouseup', mouseUpHandler);
   };
+
+  // перетаскивание копии артифакта из магазина в сумку волшебника
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      artifactsElement.style.outline = '2px dashed red';
+    }
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.preventDefault();
+    if (evt.target.tagName.toLowerCase() !== 'img') {
+      evt.target.style.backgroundColor = '';
+      var clonedArtifact = draggedItem.cloneNode(true);
+      clonedArtifact.setAttribute('draggable', false);
+      evt.target.appendChild(clonedArtifact);
+    }
+    artifactsElement.style.outline = 'none';
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    if (evt.target.tagName.toLowerCase() !== 'img' && !evt.target.hasChildNodes()) {
+      evt.target.style.backgroundColor = 'yellow';
+    }
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
+  });
 
   setupWindowUserPic.addEventListener('mousedown', dialogMoveHandler);
 })();
