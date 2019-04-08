1b658e116be76ccd470e570425df6f4b05927196
r.bykov <r.bykov@babyblog.team>
2019-04-08 14:46:10 +0300

    module6-task2

--- js/backend.js
+++ js/backend.js
@@ -2,20 +2,43 @@
 
 (function () {
   var URL = 'https://js.dump.academy/code-and-magick';
+  var STATUS_OK = 200;
+  var TIMEOUT_MS = 10000;
 
-  var upload = function (data, onSucces) {
+  var setup = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT_MS;
 
     xhr.addEventListener('load', function () {
-      onSucces(xhr.response);
+      if (xhr.status === STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+      }
     });
 
-    xhr.open('POST', URL);
-    xhr.send(data);
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
+    });
+
+    return xhr;
   };
 
   window.backend = {
-    upload: upload
+    save: function (data, onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+      xhr.open('POST', URL);
+      xhr.send(data);
+    },
+    load: function (onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+      xhr.open('GET', URL + '/data');
+      xhr.send();
+    }
   };
 })();
--- js/setup.js
+++ js/setup.js
@@ -8,39 +8,16 @@
   var userCoat = setupWindow.querySelector('.setup-wizard .wizard-coat');
   var userEyes = setupWindow.querySelector('.setup-wizard .wizard-eyes');
   var userFireball = setupWindow.querySelector('.setup-fireball-wrap');
+  var form = document.querySelector('.setup-wizard-form');
   var wizardsQuantity = 4;
-  var wizards = new Array(wizardsQuantity);
-
-  // создание магов
-  var createWizards = function (wizardsArray, number) {
-    for (var i = 0; i < number; i++) {
-      wizardsArray[i] = {
-        name: window.data.WIZARD_NAMES[window.util.getRandomNumber(0, window.data.WIZARD_NAMES.length - 1)],
-        surname: window.data.WIZARD_SURNAMES[window.util.getRandomNumber(0, window.data.WIZARD_SURNAMES.length - 1)],
-        coatColor: window.data.COAT_COLORS[window.util.getRandomNumber(0, window.data.COAT_COLORS.length - 1)],
-        eyesColor: window.data.EYE_COLORS[window.util.getRandomNumber(0, window.data.EYE_COLORS.length - 1)]
-      };
-    }
-
-    return wizardsArray;
-  };
 
   // отрисовка магов
   var renderWizard = function (wizard) {
     var wizardElement = wizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
-  };
-
-  // вставка отрисованных магов в шаблон
-  var insertWizards = function (wizardsArray, insertElement) {
-    var fragment = document.createDocumentFragment();
-    for (var j = 0; j < wizardsArray.length; j++) {
-      fragment.appendChild(renderWizard(wizardsArray[j]));
-    }
-    return insertElement.appendChild(fragment);
   };
 
   // показ похожих магов
@@ -49,12 +26,40 @@
     setupWindowWizards.classList.remove('hidden');
   };
 
-  createWizards(wizards, wizardsQuantity);
-  insertWizards(wizards, setupWindowWizardsList);
-  showSimilarWizardsWindow();
+  // вставка похожих персонажей, которые были получены с сервера, в диалоговое окно
+  var succesHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < wizardsQuantity; i++) {
+      fragment.appendChild(renderWizard(wizards[window.util.getRandomNumber(0, wizards.length)]));
+    }
+
+    setupWindowWizardsList.appendChild(fragment);
+    showSimilarWizardsWindow();
+  };
+
+  // обработка ошибок
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'position: absolute; z-index: 100; left: 0; right: 0; margin: 0 auto; font-size: 30px; text-align: center; background-color: red;';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  // отправка данных формы на сервер и скрытие окна настройки персонажа
+  var formSubmitHandler = function (evt) {
+    window.backend.save(new FormData(form), function () {
+      setupWindow.classList.add('hidden');
+    }, errorHandler);
+    evt.preventDefault();
+  };
 
   // кастомизация цветов волшебника (глаза, плащ, фаербол)
   window.colorizeElement.colorizeElement(userCoat, window.data.COAT_COLORS, window.util.fillElement);
   window.colorizeElement.colorizeElement(userEyes, window.data.EYE_COLORS, window.util.fillElement);
   window.colorizeElement.colorizeElement(userFireball, window.data.FIREBALL_COLORS, window.util.changeElementBackground);
+
+  // работа с сервером
+  window.backend.load(succesHandler, errorHandler); // загрузка данных с сервера
+  form.addEventListener('submit', formSubmitHandler); // отправка формы на сервер
 })();
